d59d85893598deb6b206207ed58e9616f730b59f
Matej Šamanić <matfunlab@gmail.com>
2018-02-25 20:30:58 +0100

    Adding project page

--- server.js
+++ server.js
@@ -58,6 +58,13 @@ app.get("/bad", (req, res) => {
   });
 });
 
+app.get("/projects", (req, res) => {
+  res.render("projects.hbs", {
+    pageTitle: "Projects page",
+    pageText: "This is my portfolio"
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
